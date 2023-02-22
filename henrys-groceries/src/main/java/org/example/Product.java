1965c2552c7620549d6119d742d5658e8295b9ba
g1ytch404 <44028291+g1ytch404@users.noreply.github.com>
2023-02-22 17:12:45 +0000

    removed unused setters and made fields final

--- henrys-groceries/src/main/java/org/example/Product.java
+++ henrys-groceries/src/main/java/org/example/Product.java
@@ -1,14 +1,15 @@
 package org.example;
+
 public enum Product {
     SOUP("soup", "tin", 0.65),
     BREAD("bread", "loaf", 0.8),
     MILK("milk", "bottle", 1.30),
     APPLE("apple", "single", 0.1);
-    private String name;
-    private String unit;
-    private double cost;
+    private final String name;
+    private final String unit;
+    private final double cost;
 
-    private Product(String name, String unit, double cost) {
+    Product(String name, String unit, double cost) {
         this.name = name;
         this.unit = unit;
         this.cost = cost;
@@ -18,23 +19,11 @@ public enum Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getUnit() {
         return unit;
     }
 
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
     public double getCost() {
         return cost;
-    }
-
-    public void setCost(double cost) {
-        this.cost = cost;
     }
 }
